ee1b94685013977abdbae92a3f818f2fa962e3c6
sam189239 <sathiyamurthi239@gmail.com>
2022-03-23 08:55:16 +0100

    update

--- test.py
+++ test.py
@@ -1,16 +1,73 @@
+
 import RPi.GPIO as GPIO
 import time
 
 #Pin definitions
-motor_pin_a=18 #board pin 12
-motor_pin_b=23 #board pin 16
+motor_pin_a=18 #board pin 12 - left fwd
+motor_pin_b=23 #board pin 16 - right fwd
+motor_pin_c=24 #board pin 18 - left back
+motor_pin_d=25 #board pin 22 - right back
+
+def fwd():
+	GPIO.output(motor_pin_a, GPIO.HIGH)
+	GPIO.output(motor_pin_b, GPIO.HIGH)
+	time.sleep(2)
+	GPIO.output(motor_pin_a, GPIO.LOW)
+	GPIO.output(motor_pin_b, GPIO.LOW)
+
+def stop():
+	GPIO.output(motor_pin_a, GPIO.LOW)
+	GPIO.output(motor_pin_b, GPIO.LOW)
+	GPIO.output(motor_pin_c, GPIO.LOW)
+	GPIO.output(motor_pin_d, GPIO.LOW)
+
+def back():
+	GPIO.output(motor_pin_c, GPIO.HIGH)
+	GPIO.output(motor_pin_d, GPIO.HIGH)
+	time.sleep(2)
+	GPIO.output(motor_pin_c, GPIO.LOW)
+	GPIO.output(motor_pin_d, GPIO.LOW)
+
+def left():
+	GPIO.output(motor_pin_c, GPIO.HIGH)
+	GPIO.output(motor_pin_b, GPIO.HIGH)
+	time.sleep(1.3)
+	GPIO.output(motor_pin_c, GPIO.LOW)
+	GPIO.output(motor_pin_b, GPIO.LOW)
+ 
+def right():
+	GPIO.output(motor_pin_d,GPIO.HIGH)
+	GPIO.output(motor_pin_a,GPIO.HIGH)
+	time.sleep(1.3)
+	GPIO.output(motor_pin_d,GPIO.LOW)
+	GPIO.output(motor_pin_a,GPIO.LOW)
 
 def main():
 	GPIO.setmode(GPIO.BCM)
 	GPIO.setwarnings(False)
 	GPIO.setup(motor_pin_a, GPIO.OUT, initial=GPIO.LOW)
 	GPIO.setup(motor_pin_b, GPIO.OUT, initial=GPIO.LOW)
+	GPIO.setup(motor_pin_c, GPIO.OUT, initial=GPIO.LOW)
+	GPIO.setup(motor_pin_d, GPIO.OUT, initial=GPIO.LOW)
+	
+	print('Starting')
+	while True:
+		i = input()
+		if i == '1':
+			stop()
+		elif i == '2':
+			fwd()
+		elif i == '3':
+			back()
+		elif i == '4':
+			left()
+		elif i == '5':
+			right()
+		else:
+			break
+	print('Done')
 
+def test():
 #	while True:
 #		print("Starting now!")	
 #		GPIO.output(motor_pin_a, GPIO.HIGH)
@@ -18,8 +75,9 @@ def main():
 
 	curr_value_pin_a=GPIO.HIGH
 	curr_value_pin_b=GPIO.HIGH
+	A = 0
 	try:
-		while True:		
+		while A<3:		
 			time.sleep(2)
 			GPIO.output(motor_pin_a, curr_value_pin_a)
 			GPIO.output(motor_pin_b, curr_value_pin_b)
@@ -28,7 +86,9 @@ def main():
 			curr_value_pin_b^=GPIO.HIGH
 			curr_value_pin_a^=GPIO.LOW
 			curr_value_pin_b^=GPIO.LOW
-			
+			A += 1
+		GPIO.output(motor_pin_a, GPIO.LOW)
+		GPIO.output(motor_pin_b, GPIO.LOW)
 	finally:
 		GPIO.cleanup()
 		
