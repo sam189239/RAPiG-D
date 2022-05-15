315e41f373a2c3b5d5cf3a06c61545c309ba807b
sam189239 <sathiyamurthi239@gmail.com>
2022-05-15 20:18:17 +0530

    update

--- flask/flask_app.py
+++ flask/flask_app.py
@@ -1,36 +1,21 @@
-from flask import Flask, render_template, request
+from flask import Flask, Response, request
 import requests
-from bs4 import BeautifulSoup
-
 
 app = Flask(__name__)
 
+#out = 'nobj'
 
-@app.route('/')
-def index():
-    return render_template('index.html')
+@app.route("/", methods=['POST'])
+def post_data():
+	global out
+	out = request.form.get('obj')
+	print("Received: {}".format(out))
+	return out
 
-
-@app.route('/suggestions')
-def suggestions():
-    text = request.args.get('jsdata')
-
-    suggestions_list = []
-
-    if text:
-        r = requests.get('http://suggestqueries.google.com/complete/search?output=toolbar&hl=ru&q={}&gl=in'.format(text))
-
-        soup = BeautifulSoup(r.content, 'lxml')
-
-        suggestions = soup.find_all('suggestion')
-
-        for suggestion in suggestions:
-            suggestions_list.append(suggestion.attrs['data'])
-
-        #print(suggestions_list)
-
-    return render_template('suggestions.html', suggestions=suggestions_list)
-
-
+@app.route("/", methods=['GET'])
+def get_data():
+	global out
+	return Response('{}'.format(out))
+	
 if __name__ == '__main__':
-    app.run(debug=True)
+	app.run(debug=True, host='0.0.0.0', port = 8000)
